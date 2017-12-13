d6bccfa3b098d1a00f9fae949c28059145431240
NewPCJan-PC\New PC Jan <jrapeldoorn@hotmail.com>
2017-12-13 15:06:34 +0100

    Finished the calculator

--- WSSCCalc/Program.cs
+++ WSSCCalc/Program.cs
@@ -10,6 +10,53 @@ namespace WSSCCalc
     {
         static void Main(string[] args)
         {
+            double firstNumber = SetNumber("Type your first number : ");
+            double secondNumber = SetNumber("Type your second number: ");
+
+            Console.WriteLine("What do you want to do with these numbers?");
+            Console.WriteLine("1 = Add");
+            Console.WriteLine("2 = Subtract");
+            int temp = int.Parse(Console.ReadLine());
+            if (temp == 1)
+            {
+                Program.Add(firstNumber, secondNumber);
+            }
+            else if (temp == 2)
+            {
+                Program.Subtract(firstNumber, secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input. Shutting down");
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void Add(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber - secondNumber;
+            Console.WriteLine("The result of "+ firstNumber + "and "+ secondNumber +"is " + result);
+        }
+
+        private static void Subtract(double firstNumber, double secondNumber)
+        {
+            double result = 0;
+            Console.WriteLine("The result of " + firstNumber + "and " + secondNumber + "is " + result);
+        }
+
+        private static double SetNumber(string outputText)
+        {
+            double parse;
+            Console.Write(outputText);
+            string tempInput = Console.ReadLine();
+            if (!double.TryParse(tempInput, out parse))
+            {
+                Console.WriteLine("Incorrect input !");
+                Console.Write(outputText);
+                tempInput = Console.ReadLine();
+            }
+            return double.Parse(tempInput);
         }
     }
 }
